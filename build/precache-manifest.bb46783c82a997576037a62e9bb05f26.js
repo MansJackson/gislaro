d0521cb9ba681e62ad51dd9a2929eda621b9848b
Måns Jackson <mans.jackson@gmail.com>
2020-07-31 20:09:44 +0200

    cleaning up

---
+++ build/precache-manifest.bb46783c82a997576037a62e9bb05f26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f970672392ee19881e37fd0a3431638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950ed467fddd84c3d527",
+    "url": "/static/js/2.cfcd3b67.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cfcd3b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea25ffdb9ea5bff3b98",
+    "url": "/static/js/main.21cd5f9f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const sendResponse = (res, data) => {
 };
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.post('/api/albums', async (req, res) => {
