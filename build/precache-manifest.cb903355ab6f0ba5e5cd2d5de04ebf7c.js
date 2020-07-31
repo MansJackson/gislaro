30b122216ea4413cd893fe4172e7740df409d40b
Måns Jackson <mans.jackson@gmail.com>
2020-07-31 20:39:55 +0200

    fixed some styles

---
+++ build/precache-manifest.cb903355ab6f0ba5e5cd2d5de04ebf7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c048f537ca8e2e47a1f7208d901215a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e7aaaaeb9097c40aeba",
+    "url": "/static/css/main.402417a0.chunk.css"
+  },
+  {
+    "revision": "88c4e0338cfafb7c05f9",
+    "url": "/static/js/2.b4ac26a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b4ac26a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7aaaaeb9097c40aeba",
+    "url": "/static/js/main.cb409e45.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb46783c82a997576037a62e9bb05f26.js"
+  "/precache-manifest.cb903355ab6f0ba5e5cd2d5de04ebf7c.js"
 );
 
 self.addEventListener('message', (event) => {
